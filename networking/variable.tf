cc937a41cde5cbaa1c5854e72cd14090155ac26a
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2024-12-05 20:30:15 -0300

    Create variable.tf

---
+++ networking/variable.tf
@@ -0,0 +1,12 @@
+#Variables block
+variable "rs_name" {
+    description = "Nome Resource group"
+    type = string
+    default = "SRMARCELO"
+}
+
+variable "rs_location" {
+    description = "Localização Resource group"
+    type = string
+    default = "eastus"
+}
